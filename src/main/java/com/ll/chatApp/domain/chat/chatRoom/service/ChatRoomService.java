48d65810064de9ff7ca57a5607619265606f927c
sik2 <bickwang123@gmail.com>
2025-01-06 17:25:14 +0900

    init Data

--- src/main/java/com/ll/chatApp/domain/chat/chatRoom/service/ChatRoomService.java
+++ src/main/java/com/ll/chatApp/domain/chat/chatRoom/service/ChatRoomService.java
@@ -1,7 +1,22 @@
 package com.ll.chatApp.domain.chat.chatRoom.service;
 
+import com.ll.chatApp.domain.chat.chatRoom.entity.ChatRoom;
+import com.ll.chatApp.domain.chat.chatRoom.repository.ChatRoomRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ChatRoomService {
+    private final ChatRoomRepository chatRoomRepository;
+
+    public ChatRoom create(String name) {
+        ChatRoom chatRoom = ChatRoom.builder()
+                                .name(name)
+                                .build();
+
+        chatRoomRepository.save(chatRoom);
+
+        return chatRoom;
+    }
 }
