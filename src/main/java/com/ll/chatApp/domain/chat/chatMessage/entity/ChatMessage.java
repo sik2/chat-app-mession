c78cf7d60fb8308c24b0f79ebd41fcf6993b3e5c
sik2 <bickwang123@gmail.com>
2025-01-06 16:37:21 +0900

    entity

--- src/main/java/com/ll/chatApp/domain/chat/chatMessage/entity/ChatMessage.java
+++ src/main/java/com/ll/chatApp/domain/chat/chatMessage/entity/ChatMessage.java
@@ -1,7 +1,9 @@
 package com.ll.chatApp.domain.chat.chatMessage.entity;
 
+import com.ll.chatApp.domain.chat.chatRoom.entity.ChatRoom;
 import com.ll.chatApp.global.jpa.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -16,4 +18,8 @@ public class ChatMessage extends BaseEntity {
     public ChatMessage() {
 
     }
+    private String writerName;
+    private String content;
+    @ManyToOne
+    private ChatRoom chatRoom;
 }
--- src/main/java/com/ll/chatApp/domain/chat/chatRoom/entity/ChatRoom.java
+++ src/main/java/com/ll/chatApp/domain/chat/chatRoom/entity/ChatRoom.java
@@ -1,11 +1,15 @@
 package com.ll.chatApp.domain.chat.chatRoom.entity;
 
+import com.ll.chatApp.domain.chat.chatMessage.entity.ChatMessage;
 import com.ll.chatApp.global.jpa.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+
+import java.util.List;
 
 @Entity
 @Setter
@@ -16,4 +20,8 @@ public class ChatRoom  extends BaseEntity {
     public ChatRoom () {
 
     }
+    private String name;
+
+    @OneToMany
+    private List<ChatMessage> chatMessages;
 }
