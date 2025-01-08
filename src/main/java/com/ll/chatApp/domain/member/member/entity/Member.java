61f9083f8c64a2cd1467dacddbf99f9f92c00ec5
sik2 <bickwang123@gmail.com>
2025-01-08 12:18:06 +0900

    join member

---
+++ src/main/java/com/ll/chatApp/domain/member/member/entity/Member.java
@@ -0,0 +1,18 @@
+package com.ll.chatApp.domain.member.member.entity;
+
+import com.ll.chatApp.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class Member extends BaseEntity {
+    String username;
+    String password;
+}
