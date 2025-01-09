8b03f7f21c5c04f9e6b8b6b2aedfc9e085919916
sik2 <bickwang123@gmail.com>
2025-01-09 17:36:02 +0900

    complete queryDSL

--- src/main/java/com/ll/chatApp/domain/article/article/repository/ArticleRepositoryCustom.java
+++ src/main/java/com/ll/chatApp/domain/article/article/repository/ArticleRepositoryCustom.java
@@ -1,4 +1,11 @@
 package com.ll.chatApp.domain.article.article.repository;
 
+import com.ll.chatApp.domain.article.article.entity.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface ArticleRepositoryCustom {
+    Page<Article> search(List<String> kwTypes, String kw, Pageable pageable);
 }
